deb333054c2ba7b237fdc386ec9e7c8599792b66
Mohamed Shahul Hameed <thameemsh@email.com>
2021-11-02 15:50:28 -0400

    Refactor typewriter.js

--- typewriter.js
+++ typewriter.js
@@ -1,6 +1,6 @@
 const sentence = "hello there from lighthouse labs.";
 
-const sent = sentence + "\n"
+const sent = sentence + "\n";
 for (let i in sent) {
- setTimeout(()=>process.stdout.write(sent[i]),i*50);
+  setTimeout(()=>process.stdout.write(sent[i]),i * 50);
 }
