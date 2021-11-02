8c02507ce31b2c224305ebebf6075548d1a7ca7f
Mohamed Shahul Hameed <thameemsh@email.com>
2021-11-02 15:46:11 -0400

    Fix Error

--- typewriter.js
+++ typewriter.js
@@ -1,14 +1,6 @@
 const sentence = "hello there from lighthouse labs.";
-sent = sentence + "\n"
+
+const sent = sentence + "\n"
 for (let i in sent) {
  setTimeout(()=>process.stdout.write(sent[i]),i*50);
 }
-
-// const sentence = " The future belongs to those who believe in the beauty of their dreams.\n";
-// let letter = sentence.slice(" ");
-// let delay = 500;
-//  for(const char of letter){
-//    setTimeout(()=>{
-//  process.stdout.write(char);},delay);
-//  delay += 50;
-//  };
